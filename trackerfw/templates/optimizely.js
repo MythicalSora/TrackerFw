0739af4aa281ec87bad1403f55d32c88878adc92
Dillen Meijboom <info@dmeijboom.nl>
2018-02-04 23:54:43 +0100

    Added more trackers

---
+++ trackerfw/templates/optimizely.js
@@ -0,0 +1,6 @@
+window.performance = {};
+window.optimizely = {
+    push: function() {
+        
+    }
+};
---
+++ trackerfw/templates/vwo.js
@@ -0,0 +1,5 @@
+_vwo_code.sT = true;
+
+window.VWO = window.VWO || [];
+window.VWO.data = {};
+window.VWO.data.as = "rec2.visualwebsiteoptimizer.com";
